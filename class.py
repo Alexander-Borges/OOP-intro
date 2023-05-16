e98210bbf47371c946de079bfc3922df9f988d0b
Alexander Borges <borges2721@gmail.com>
2023-05-15 20:12:34 -0400

    first commit

---
+++ class.py
@@ -0,0 +1,15 @@
+class User:
+    def __init__(self):
+        self.first_name = "Ada"
+        self.last_name = "Lovelace"
+        self.age = 42
+
+print("Hello")
+
+user_ada = User()
+print(user_ada.first_name)
+print(user_ada.last_name)
+print(user_ada.age)
+
+user_2 = User()
+print(user_2.first_name)
